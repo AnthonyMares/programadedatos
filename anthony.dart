ef4616c1d886644beb18396269f16c4bddf11f92
AnthonyMares <mareseleys@gmail.com>
2026-02-23 17:15:07 -0500

    nombres

---
+++ anthony.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+void main() {
+print("Escriba su primer nombre:");
+  String a = stdin.readLineSync() ?? "";
+  print("Escriba su segundo nombre:");
+  String b = stdin.readLineSync() ?? "";
+  }
